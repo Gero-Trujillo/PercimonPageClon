80d4f294de86e1d0fc7e0563a65346291d1a519e
Gero <gtutos3@gmail.com>
2024-09-01 20:17:36 -0500

    Fixed Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gero-trujillo.github.io',
+  site: 'https://Gero-Trujillo.github.io',
   base: '/PercimonPageClon',
   integrations: [tailwind(), react()]
 });
